95023d0e5d5088d7194395a49861858b1e5fb6a3
Roman Alenskiy <kuperman88@gmail.com>
2018-03-22 20:20:35 +0300

    getting rid of class variables

--- web_hangman.rb
+++ web_hangman.rb
@@ -7,65 +7,65 @@ get '/' do
     redirect '/start'
 end
 
-current_dictionary = ""
-is_new_game = false
-@@dictionary = ""
-@@secret_word = []
-@@fields = " "
-@@suitable_letters = []
-@@used_letters = []
-@@number_of_attempts = 6
-@@message = ""
+set :current_dictionary, ""
+set :is_new_game, false
+set :dictionary, ""
+set :secret_word, []
+set :fields, " "
+set :suitable_letters, []
+set :used_letters, []
+set :number_of_attempts, 6
+set :message, ""
 
 get '/start' do
     @file_names = find_files("dictionary") 
 
-    if !(params['file_name'].nil?) && (@@dictionary.empty? || current_dictionary != params['file_name'])
-        @@dictionary = load_file("dictionary", params['file_name'])
+    if !(params['file_name'].nil?) && (settings.dictionary.empty? || settings.current_dictionary != params['file_name'])
+        settings.dictionary = load_file("dictionary", params['file_name'])
         game_refresh
-        current_dictionary = params['file_name']
+        settings.current_dictionary = params['file_name']
     end
 
-    if !(@@dictionary.empty?) && (is_new_game)
+    if !(settings.dictionary.empty?) && (settings.is_new_game)
         game_refresh
-        is_new_game = false
+        settings.is_new_game = false
     end
 
-    if !(params['guess'].nil?) && (@@number_of_attempts != 0) && (@@fields != @@secret_word.join)
-        @@message = "Choose the dictionary first!" if @@fields == " "
-        @@suitable_letters = @@secret_word.each_index.select {|index| @@secret_word[index] == params['guess']}
-        push_used_letter(@@used_letters, params['guess'])
-        unless @@suitable_letters.empty?
-            step(@@suitable_letters, @@fields, @@secret_word)
+    if !(params['guess'].nil?) && (settings.number_of_attempts != 0) && (settings.fields != settings.secret_word.join)
+        settings.message = "Choose the dictionary first!" if settings.fields == " "
+        settings.suitable_letters = settings.secret_word.each_index.select {|index| settings.secret_word[index] == params['guess']}
+        push_used_letter(settings.used_letters, params['guess'])
+        unless settings.suitable_letters.empty?
+            step(settings.suitable_letters, settings.fields, settings.secret_word)
         else
-            @@number_of_attempts -= 1
+            settings.number_of_attempts -= 1
         end
         gameover_check
     end
 
-    erb :start, :locals => {:file_name => params['file_name']}
+    erb :start, :locals => {:file_name => params['file_name'], :dictionary => settings.dictionary, :secret_word => settings.secret_word, :fields => settings.fields, :suitable_letters => settings.suitable_letters, :used_letters => settings.used_letters, :number_of_attempts => settings.number_of_attempts, :message => settings.message}
 end
 
 post '/new' do
-    @@message = "Choose the dictionary first!" if @@fields == " "
-    is_new_game = true
+    settings.message = "Choose the dictionary first!" if settings.fields == " "
+    settings.is_new_game = true
     redirect "/start"
 end
 
 def game_refresh
-    @@secret_word = choose_word(@@dictionary)
-    @@fields = "_" * @@secret_word.size
-    @@used_letters = []
-    @@number_of_attempts = 6
-    @@message = ""
+    settings.secret_word = choose_word(settings.dictionary)
+    settings.fields = "_" * settings.secret_word.size
+    settings.used_letters = []
+    settings.number_of_attempts = 6
+    settings.message = ""
 end
 
 def gameover_check
-    if @@number_of_attempts == 0
-        @@message = "Game over! (answer was: '#{@@secret_word.join}')"
+    if settings.number_of_attempts == 0
+        settings.message = "Game over! (answer was: '#{settings.secret_word.join}')"
     end
 
-    if @@fields == @@secret_word.join
-        @@message = "YOU WIN!"
+    if settings.fields == settings.secret_word.join
+        settings.message = "YOU WIN!"
     end
 end
