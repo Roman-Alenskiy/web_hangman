479fa8362b67fc4ed5c53e0ba4b21d16699db276
Roman Alenskiy <kuperman88@gmail.com>
2018-03-24 15:47:32 +0300

    push helper methods to module hangman.rb

--- hangman.rb
+++ hangman.rb
@@ -1,14 +1,14 @@
 module Hangman
 
-    def step(suitable_letters, fields, secret_word)
-        suitable_letters.each do |index|
-            fields[index] = secret_word[index]
+    def step(session)
+        session['suitable_letters'].each do |index|
+            session['fields'][index] = session['secret_word'][index]
         end
     end
 
-    def push_used_letter(used_letters, letter)
-        used_letters << letter
-        used_letters.uniq!
+    def push_used_letter(session, letter)
+        session['used_letters'] << letter
+        session['used_letters'].uniq!
     end
 
     def choose_word(current_dictionary)
@@ -29,6 +29,37 @@ module Hangman
         
     def load_file(catalog_name, file_name)
         return File.read("public/" + catalog_name + "/" + file_name)
+    end
+
+    def game_refresh(session)
+        session['secret_word'] = choose_word(session['current_dictionary'])
+        session['fields'] = "_" * session['secret_word'].size
+        session['used_letters'] = []
+        session['number_of_attempts'] = 6
+        session['message'] = ""
+    end
+    
+    def gameover_check(session)
+        if session['number_of_attempts'] == 0
+            session['message'] = "Game over! (answer was: '#{session['secret_word'].join}')"
+        end
+    
+        if session['fields'] == session['secret_word'].join
+            session['message'] = "YOU WIN!"
+        end
+    end
+    
+    def initial_assignment(session)
+        if session['secret_word'].nil?
+            session['current_dictionary'] = ""
+            session['is_new_game'] = false
+            session['secret_word'] = []
+            session['fields'] = " "
+            session['suitable_letters'] = []
+            session['used_letters'] = []
+            session['number_of_attempts'] = 6
+            session['message'] = ""
+        end
     end
 
     def foo
--- web_hangman.rb
+++ web_hangman.rb
@@ -10,31 +10,31 @@ end
 enable :sessions
 
 get '/start' do
-    initial_assignment
+    initial_assignment(session)
     @file_names = find_files("dictionary") 
 
     if !(params['file_name'].nil?) && (session['current_dictionary'] != params['file_name'])
         session['current_dictionary'] = params['file_name']
-        game_refresh
+        game_refresh(session)
     end
 
     if !(session['current_dictionary'].empty?) && (session['is_new_game'])
-        game_refresh
+        game_refresh(session)
         session['is_new_game'] = false
     end
 
     if !(params['guess'].nil?) && (session['number_of_attempts'] != 0) && (session['fields'] != session['secret_word'].join)
         session['suitable_letters'] = session['secret_word'].each_index.select {|index| session['secret_word'][index] == params['guess']}
         if !(session['suitable_letters'].empty?)
-            step(session['suitable_letters'], session['fields'], session['secret_word'])
-            push_used_letter(session['used_letters'], params['guess'])
+            step(session)
+            push_used_letter(session, params['guess'])
         elsif session['current_dictionary'].empty?
             session['message'] = "Choose the dictionary first!"
         else
             session['number_of_attempts'] -= 1
-            push_used_letter(session['used_letters'], params['guess'])
+            push_used_letter(session, params['guess'])
         end
-        gameover_check
+        gameover_check(session)
     end
 
     erb :start, :locals => {:current_dictionary => session['current_dictionary'], :dictionary => session['dictionary'], :secret_word => session['secret_word'], :fields => session['fields'], :suitable_letters => session['suitable_letters'], :used_letters => session['used_letters'], :number_of_attempts => session['number_of_attempts'], :message => session['message']}
@@ -44,35 +44,4 @@ post '/new' do
     session['message'] = "Choose the dictionary first!" if session['fields'] == " "
     session['is_new_game'] = true
     redirect "/start"
-end
-
-def game_refresh
-    session['secret_word'] = choose_word(session['current_dictionary'])
-    session['fields'] = "_" * session['secret_word'].size
-    session['used_letters'] = []
-    session['number_of_attempts'] = 6
-    session['message'] = ""
-end
-
-def gameover_check
-    if session['number_of_attempts'] == 0
-        session['message'] = "Game over! (answer was: '#{session['secret_word'].join}')"
-    end
-
-    if session['fields'] == session['secret_word'].join
-        session['message'] = "YOU WIN!"
-    end
-end
-
-def initial_assignment
-    if session['secret_word'].nil?
-        session['current_dictionary'] = ""
-        session['is_new_game'] = false
-        session['secret_word'] = []
-        session['fields'] = " "
-        session['suitable_letters'] = []
-        session['used_letters'] = []
-        session['number_of_attempts'] = 6
-        session['message'] = ""
-    end
 end
